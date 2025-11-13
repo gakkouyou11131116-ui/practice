48142cae222f783654ca28e9e35acd468fa1c197
gakkouyou11131116 <gakkouyou11131116@gmail.com>
2025-11-13 18:23:47 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class Book extends TangibleAsset{
+	private String isbn;
+	public Book(String name,String color,String isbn){
+		super(name,price,color);
+		this.isbn = isbn;
+	}
+	public String getisbn(){return this.isbn;}
+}
+
